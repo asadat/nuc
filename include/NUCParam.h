835cb229be3a8d4b0b7c553f0fd2c3a5cf3736a3
Abbas <sas21@sfu.ca>
2014-04-27 17:10:44 -0700

    param class

---
+++ include/NUCParam.h
@@ -0,0 +1,14 @@
+#ifndef _NUCPARAM_
+#define _NUCPARAM_
+
+#define D2R 3.14/180
+
+class NUCParam
+{
+public:
+    static double area_rotation;
+    static double cx;
+    static double cy;
+};
+
+#endif
---
+++ src/NUCParam.cpp
@@ -0,0 +1,5 @@
+#include "NUCParam.h"
+
+double NUCParam::area_rotation = 0;
+double NUCParam::cx = 0;
+double NUCParam::cy = 0;
