b7fe5d18ec297cce89d0cf67d6660a9af6e65bb5
Abbas <sas21@sfu.ca>
2014-08-20 11:34:35 -0700

    added the result generation scripts

---
+++ experiments/result-table.sh
@@ -0,0 +1,13 @@
+if [ ! -e "stat" ]
+then
+	g++ stat.cpp -o stat
+fi
+
+for PATCH in 1 2 3; do
+	for PERCENT in 10 20 30 40 50 60 70 80 90; do
+		cat results | grep $1 | grep "PATCHES:$PATCH" | grep "PERCENT:$PERCENT" | sed -e "s/.*LENGTH //g" > tmp
+		MEAN_VAR=$(./stat tmp)
+		#echo "$1 PATCHES:$PATCH PERCENT:$PERCENT $MEAN_VAR"
+	        echo "$PATCH $PERCENT $MEAN_VAR"
+	done
+done
