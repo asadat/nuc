fff8381fde0f846c21bc43dc2ed91e72fd430d14
asadat <abbas.sadat@gmail.com>
2014-08-20 14:28:17 -0700

    experiment results

--- experiments/run.sh
+++ experiments/run.sh
@@ -5,12 +5,16 @@ roslaunch pelican_ctrl sim.launch &
 sleep 5.0
 
 echo > results
-for PATCHES in 1  
+for PATCHES in 2 3 4  
 do
 	for STRATEGY in 1 2 3 
 	do
-		for PERCENT in 10 20 30 40
+		for PERCENT in 10 20 30 40  
 		do
+			#if [ $PATCHES -gt 1 ] && [ $PERCENT -gt 40 ]; then 
+			#	continue
+			#fi
+
 			for i in 1 2 3 4 5 6 7 8 9 10
 			do 
 			    cat param.yaml > ../launch/batchrun_param.yaml
@@ -24,8 +28,8 @@ do
 
 			    if [ $STRATEGY -eq 2 ]; then
 				    echo "strategy: sc"
-                    echo "strategy: sc" >> ../launch/batchrun_param.yaml
-                fi
+                    		    echo "strategy: sc" >> ../launch/batchrun_param.yaml
+                	    fi
 
 			    if [ $STRATEGY -eq 3 ]; then
 				    echo "strategy: hi"
