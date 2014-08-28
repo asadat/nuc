5d1416e329bef189b91671b80dad42a9054ac64c
asadat <abbas.sadat@gmail.com>
2014-08-28 10:09:48 -0700

    experiment params

--- experiments/result-table.sh
+++ experiments/result-table.sh
@@ -3,9 +3,11 @@ then
 	g++ stat.cpp -o stat
 fi
 
-for PATCH in 1; do
-	for PERCENT in 10 20 30 40 50 60 70 80 90; do
-		cat results | grep $1 | grep "PATCHES:$PATCH" | grep "PERCENT:$PERCENT" | sed -e "s/.*LENGTH //g" > tmp
+for PATCH in `seq 1 1 4`
+do
+	for PERCENT in `seq 10 10 90` 
+do
+		cat $1 | grep $2 | grep "PATCHES:$PATCH" | grep "PERCENT:$PERCENT" | sed -e "s/.*LENGTH //g" > tmp
 		MEAN_VAR=$(./stat tmp)
 		#echo "$1 PATCHES:$PATCH PERCENT:$PERCENT $MEAN_VAR"
 	        echo "$PATCH $PERCENT $MEAN_VAR"
