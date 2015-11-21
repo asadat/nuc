4c033b1404eb57f6fa120c9c256915fcdff0e705
Abbas <abbas.sadat@gmail.com>
2015-11-21 11:52:02 -0800

    batch test run

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,47 +1,44 @@
 #!/bin/bash
 
-#export ROS_MASTER_URI=http://localhost:22322
-#roscore -p 22322 &
-#sleep 5.0
 localros
 roslaunch pelican_ctrl sim.launch &
 sleep 5.0
-
+cat ../launch/batchrun_param.yaml
 echo > results
-for PATCHES in `seq 1 1 1` 
-do
+#for PATCHES in `seq 1 1 1` 
+#do
 	for STRATEGY in `seq 3 1 3`  
 	do
-		for PERCENT in `seq 10 20 90` 
-		do		
+		#for PERCENT in `seq 10 20 90` 
+		#do		
 
-			for i in `seq 1 2 10`
-			do 
+			#for i in `seq 1 2 10`
+			#do 
 			    cat param.yaml > ../launch/batchrun_param.yaml
-			    echo "patches: $PATCHES" >> ../launch/batchrun_param.yaml
-			    echo "percent_interesting: $PERCENT" >> ../launch/batchrun_param.yaml
+			    #echo "patches: $PATCHES" >> ../launch/batchrun_param.yaml
+			    #echo "percent_interesting: $PERCENT" >> ../launch/batchrun_param.yaml
 		
 			    if [ $STRATEGY -eq 1 ]; then
-				    echo "strategy: df"
-				    echo "strategy: df" >> ../launch/batchrun_param.yaml
+				    echo "policy: greedy"
+				    echo "policy: greedy" >> ../launch/batchrun_param.yaml
 			    fi
 
 			    if [ $STRATEGY -eq 2 ]; then
-				    echo "strategy: sc"
-                    		    echo "strategy: sc" >> ../launch/batchrun_param.yaml
+				    echo "policy: delayed_greedy"
+                    		    echo "policy: delayed_greedy" >> ../launch/batchrun_param.yaml
                 	    fi
 
 			    if [ $STRATEGY -eq 3 ]; then
-				    echo "strategy: hi"
-                    		    echo "strategy: hi" >> ../launch/batchrun_param.yaml
+				    echo "policy: delayed"
+                    		    echo "policy: delayed" >> ../launch/batchrun_param.yaml
                  	    fi
                  
-			    echo "run:$i strategy:$STRATEGY patches:$PATCHES percent:$PERCENT" 
-			    roslaunch nuc batchrun.launch  | grep "STRATEGY" >> results
+			    #echo "run:$i strategy:$STRATEGY patches:$PATCHES percent:$PERCENT" 
+			    roslaunch nuc batchrun.launch  #| grep "resolution" 
  			    
-			done
-		done
-	done
+			#done
+		#done
+	#done
 done
 
 killall roslaunch
